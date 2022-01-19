753b2faf60812d91acd91321c6461375296b0e4b
Heasunglee <83310860+Heasunglee@users.noreply.github.com>
2022-01-19 10:24:53 -0700

    Update new.py

--- new.py
+++ new.py
@@ -71,7 +71,7 @@ class Target():
 
     targetarray = []
     def gentarget():
-        for x in range(20):
+        for x in range(10):
             Target.targetarray.append(Target(0,-85,85,40,screen,False))
             Target.targetarray.append(Target(98,-85,85,40,screen,False))
             Target.targetarray.append(Target(191,-85,85,40,screen,False))
@@ -139,7 +139,6 @@ while True:
                 block.x = 284
             if event.key == pygame.K_SPACE:
                 gameover = False
-                score = 0
 
     if gameover == False: 
         
@@ -176,13 +175,13 @@ while True:
                 gameover = True
                 break
 
+
     else:
         #Drawing Gameover Screen
-        screen.fill((100,0,0))
+        screen.fill((0,0,0))
         label = font.render("Game Over", 1, (255,255,255))
         label1 = font1.render("Press Space To Restart", 1, (255,255,255))
-        label2 = font1.render(str(score), 1,(255,255,255))
-        label3 = font1.render("Your Score:", 1,(255,255,255))
+        label2 = font1.render("Score:", 1,(255,255,255))
         screen.blit(label, (35,50))
         screen.blit(label1, (30,500))
         screen.blit(label2, (30,400))
@@ -192,6 +191,7 @@ while True:
             score = 0
             block.x = 400
             for target in Target.targetarray:
+<<<<<<< HEAD
                 target.falling = False
         reset()
 
@@ -200,6 +200,11 @@ while True:
         screen.blit(label3, (90,400))
         for target in Target.targetarray:
             target.y-=50
+=======
+                target.y = -85
+        reset()
+
+>>>>>>> parent of 14fd172 (Update new.py)
 
     pygame.display.update()
     FramePerSec.tick(FPS)
